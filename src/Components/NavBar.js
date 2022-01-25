63583afa8ebfc33d92740e60b28275edf38b0ceb
fernando molina <fer.molina@live.com.ar>
2022-01-24 23:09:36 -0300

    dos

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+
+const NavBar  = () => {
+    return (
+        <nav className="labarra">
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    )    
+}
+
+export default NavBar;
